b879a4032a7dfb51f03df0b3356af1e1d6f8dce5
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2021-07-17 16:31:00 +0600

    Subtraction

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -1,9 +1,15 @@
 public class SimpleCalculator {
     public static void main(String[] args) {
         sum(10,5);
+        sub(20,10);
     }
     public static void sum(int a, int b){
         int summation=a+b;
         System.out.println(summation);
     }
+    public static void sub(int a, int b){
+        int subtraction=a-b;
+        System.out.println(subtraction);
+    }
+
 }
