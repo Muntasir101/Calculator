dec0be9bbd6ae00d8afe0d5313e50a5f5958f2af
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2021-07-17 16:18:05 +0600

    Summation

---
+++ src/main/java/SimpleCalculator.java
@@ -0,0 +1,9 @@
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        sum(10,5);
+    }
+    public static void sum(int a, int b){
+        int summation=a+b;
+        System.out.println(summation);
+    }
+}
