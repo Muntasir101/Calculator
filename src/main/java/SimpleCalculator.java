e6c3559b0796495c21ba8ca561fe332e9475fdc5
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2021-07-17 16:45:30 +0600

    Multiply

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -2,6 +2,7 @@ public class SimpleCalculator {
     public static void main(String[] args) {
         sum(10,5);
         sub(20,10);
+        mul(5,5);
     }
     public static void sum(int a, int b){
         int summation=a+b;
@@ -10,6 +11,10 @@ public class SimpleCalculator {
     public static void sub(int a, int b){
         int subtraction=a-b;
         System.out.println(subtraction);
+    }
+    public static void mul(int a, int b){
+        int multiply=a*b;
+        System.out.println(multiply);
     }
 
 }
